ac76f50aef2f86dce26b4223da11a5b4eed764f1
Juan Alejandro Espejo Vacaflores <juanalejandroespejovacaflores@gmail.com>
2022-06-29 23:25:41 -0400

    feat(common): update register page

---
+++ src/ui/components/register/stories/Label.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Label from "../subcomponents/Label/Label";
+export default {
+    title: 'ui/components/register/subcomponents/Label',
+    component: Label
+}
+const Template = args => <Label  {...args}/>
+export const DefaultLabel = Template.bind({});
+DefaultLabel.args = {
+    text: 'Soy un Label :D'
+}
