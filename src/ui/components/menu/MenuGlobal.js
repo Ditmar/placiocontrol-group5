c7950aa45d7fcd838a78ba727f78106b40128886
Juan Alejandro Espejo Vacaflores <juanalejandroespejovacaflores@gmail.com>
2022-07-08 13:41:15 -0400

    feature(common): add componentes for integration branch of menu

---
+++ src/ui/components/menu/MenuGlobal.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import { Wrapper } from './wrapper/Wrapper';
+import './MenuGlobal.css'
+export const MenuGlobal = () => {
+    return (
+        <div className='container-menu-global'>
+            <Wrapper/>
+        </div>
+    )
+}
