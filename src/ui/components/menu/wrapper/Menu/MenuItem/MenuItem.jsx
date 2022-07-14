a58d0b7a40e3e7b0d90848e8c0d95288ab6f6dd3
Bryan Kenny Gutierrez Conde <bryangutierrezconde@gmail.com>
2022-07-14 01:11:06 -0400

    ups

--- src/ui/components/menu/wrapper/Menu/MenuItem/MenuItem.jsx
+++ src/ui/components/menu/wrapper/Menu/MenuItem/MenuItem.jsx
@@ -4,7 +4,7 @@ export const MenuItem = ({title, image, param}) => {
     return (
         <div className={ param ?  'item-collapsed' : 'item'}>
             <a> 
-                <div className={ param ?  'icon-collapsed' : 'iconbug'}><img className='image-item' src={image}/></div>
+                <div className={ param ?  'icon-collapsed' : 'icon'}><img className='image-item' src={image}/></div>
                 <div className={ param ?  'title-collapsed' : 'title'}>{title}</div>
             </a>
         </div>
